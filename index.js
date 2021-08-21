b4d5c3206139ce3f0da769c63be28a598979a8d3
Hudson-Pereira <79792101+Hudson-Pereira@users.noreply.github.com>
2021-08-21 01:47:44 +0000

    funcoes

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-//receber do usuario um numero e responder se eh par ou impar.
-let n = parseInt(prompt('Digite um número'));
+//Funções são rotinas do meu programa.
+//Rotinas são ações repetitivas, que podem sintetizar em uma função, para fazer repetidas vezes, sem ter que criar as mesmas linhas de código.
 
-if (n % 2 === 0) {
-  console.log('O número é par.');
-  } else if(n % 2 === 1) { 
-    console.log('O número é impar.');
-  } else {
-    console.log('Digite um numero válido.');
-  };
+//Sem funcao
+
+let n2 = 6;
+let n3 = 3;
+let somar = n2 + n3;
+console.log(somar);
+
+// com funcao
+//criar a funcao e chamar ela depois com os valores
+function soma(x,y){
+  
+  return x+y;
+};
+console.log(soma(4,40));
+console.log(soma(87,3));
+console.log(soma(7,3));
+console.log(soma(8,54));
+
+function div(g,h){
+  return g/h;
+};
+
+console.log(div(5,2));
+console.log(div(33,3));
+
+function sub(a,b){
+  return a-b;
+};
+
+console.log(sub(10,5));
+console.log(sub(546,43));
---
+++ index3.js
@@ -0,0 +1,10 @@
+//receber do usuario um numero e responder se eh par ou impar.
+let n = parseInt(prompt('Digite um número'));
+
+if (n % 2 === 0) {
+  console.log('O número é par.');
+  } else if(n % 2 === 1) { 
+    console.log('O número é impar.');
+  } else {
+    console.log('Digite um numero válido.');
+  };
